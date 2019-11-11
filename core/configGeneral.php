46cfc7713dbe73b20463039a1d9b875004b1ac11
eudomars <leomarsepulvedar@gmail.com>
2019-11-11 09:51:08 -0430

    se lñe coloco la url por defecto q le corresponde

--- core/configGeneral.php
+++ core/configGeneral.php
@@ -1,5 +1,5 @@
 <?php
-    const SERVERURL = 'http://localhost/proyectos/CURSOS/plantilla/';
+    const SERVERURL = 'http://localhost/reposo/';
     const COMPANY = 'Sistema';
 
     date_default_timezone_set('America/Caracas');
