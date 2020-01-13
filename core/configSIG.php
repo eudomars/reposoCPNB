9411d57b3f116cb91e1412238036aac6609698e7
eudomars <leomarsepulveda@gmail.com>
2020-01-13 16:09:23 -0430

    Archivos fijos no rasteados

--- core/configSIG.php
+++
@@ -1,9 +0,0 @@
-<?php
-const SERVERS = "localhost options='--client_encoding=UTF8'";
-// const SERVER = "localhost options='--client_encoding=UTF8'";
-const DBNAMES = 'sigefirrhh';
-const DBUSERS = 'postgres';
-const DBPASSS = 'eudomars2004';
-const DBPORTS = 5433; // Solo para base de datos PostgreSQL
-
-const SGBDS = 'pgsql:host='.SERVERS.';port='.DBPORTS.';dbname='.DBNAMES;
